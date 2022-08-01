fc74db84137a74419571313151ef825b024235d6
Himanshi Arora <arorahimanshi0722@gmail.com>
2022-08-01 16:37:10 +0530

    styled show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,6 +5,7 @@ import Details from "../components/show/Details";
 import Seasons from "../components/show/Seasons";
 import ShowMainData from "../components/show/ShowMainData";
 import { apiGet } from "../misc/config";
+import { InfoBlock, ShowPageWrapper } from "./Show.styled";
 
 const reducer = (prevState, action) => {
   // because every action has type key , hence we can use switch function
@@ -68,22 +69,24 @@ const Show = () => {
     return <div> {error} Error Occured</div>;
   }
   return (
-    <div>
+    <ShowPageWrapper >
       <ShowMainData image={show.image} name={show.name} rating={show.rating} summary={show.summary} tags={show.genres} />
 
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details status={show.status} network={show.network} premiered={show.premiered} />
-      </div>
-      <div>
+      </InfoBlock>
+
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons seasons={show._embedded.seasons} />
-      </div>
-      <div>
+      </InfoBlock>
+      
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast={show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
